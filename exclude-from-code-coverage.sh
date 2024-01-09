f3cc6ab811a3dcfd5d3e0e2413a585f53ba03024
Gustavo Martins <gustavo.martins@mnwprodu-GMLX.meganews.local>
2024-01-09 11:45:43 -0300

    Changing again

--- exclude-from-code-coverage.sh
+++ exclude-from-code-coverage.sh
@@ -1,4 +1,4 @@
 while read p || [ -n "$p" ]
 do
-echo -e "$(cat ./coverage.out)\n$p:1.1,1.2 1 1" > ./coverage.out
+echo -e "$(cat ./cover.out)\n$p:1.1,1.2 1 1" > ./cover.out
 done < ./exclude-from-code-coverage.txt
